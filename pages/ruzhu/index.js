ebf2b1893007e995f2b6dcff4c5f0605ea769957
WuGuoQing <745272308@qq.com>
2018-12-20 22:50:42 +0800

    加入样式

--- pages/ruzhu/index.js
+++ pages/ruzhu/index.js
@@ -1,18 +1,28 @@
 // pages/ruzhu/index.js
+var QQMapWX = require('../../mapsdk/qqmap-wx-jssdk');
+var qqmapsdk;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    files: []
+    files: [],
+    jcfiles: [],
+    mmimg:'',
+    yyimg: '',
+    address:'',
+    ad_info:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 实例化API核心类
+    qqmapsdk = new QQMapWX({
+      key: '6CXBZ-QNVRU-ITIVQ-4ALSI-WV7QQ-KHFNQ' // 必填
+    });
   },
 
   /**
@@ -63,6 +73,34 @@ Page({
   onShareAppMessage: function () {
 
   },
+  mmchooseImage:function(e){
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          mmimg:res.tempFilePaths
+        });
+      }
+    })
+  },
+  yychooseImage:function(){
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          yyimg: res.tempFilePaths
+        });
+      }
+    })
+  },
   chooseImage: function (e) {
     var that = this;
     wx.chooseImage({
@@ -72,6 +110,19 @@ Page({
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         that.setData({
           files: that.data.files.concat(res.tempFilePaths)
+        });
+      }
+    })
+  },
+  jcchooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          jcfiles: that.data.jcfiles.concat(res.tempFilePaths)
         });
       }
     })
@@ -90,5 +141,35 @@ Page({
     this.setData({
       files: files
     });
+  },
+  chooseLocation: function (e) {
+    var that = this;
+    wx.chooseLocation({
+      success: function (res) {
+
+        // 调用接口
+        qqmapsdk.reverseGeocoder({
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          },
+          success: function (res) {
+            console.log(res);
+            that.setData({
+              ad_info: res.result.ad_info,
+              address: res.result.address_component.street + res.result.address_component.street_number,
+              pcd: res.result.address_component.province + res.result.address_component.city + res.result.address_component.district
+            });
+          },
+          fail: function (res) {
+            console.log(res);
+          },
+          complete: function (res) {
+            console.log(res);
+          }
+        });
+      },
+    })
+
   }
 })

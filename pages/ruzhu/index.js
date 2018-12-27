4c26f3ad9b409c66193a0dcfb08b91f5312444ae
jackhoop <wgqhlzds@gmail.com>
2018-12-27 18:23:42 +0800

    底部菜单

--- pages/ruzhu/index.js
+++ pages/ruzhu/index.js
@@ -84,20 +84,14 @@ Page({
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        wx.uploadFile({
-          url: app.globalData.serverUrl + "/wx/media/" + app.globalData.appid + "/uploadImg",
-          filePath: res.tempFilePaths[0],
-          name: 'img',
-          formData: {
-            'dataType': 'bus'
-          },
-          success: function (res) {
-            console.log(res)
-            //do something
-          }
-        })
-    
+        var formData={
+          "dataType":"bus"
+        }
+        that.imgUpdate(res.tempFilePaths, formData,function(data){
+          that.setData({
+            mmimg: data.path
+          });
+        });
       }
     })
   },
@@ -108,9 +102,15 @@ Page({
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
+
+        var formData = {
+          "dataType": "bus"
+        }
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
-          yyimg: res.tempFilePaths
+        that.imgUpdate(res.tempFilePaths, formData, function (data) {
+          that.setData({
+            yyimg: data.path
+          });
         });
       }
     })
@@ -121,9 +121,14 @@ Page({
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
+        var formData = {
+          "dataType": "bus"
+        }
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
-          files: that.data.files.concat(res.tempFilePaths)
+        that.imgUpdate(res.tempFilePaths, formData, function (data) {
+          that.setData({
+            files: that.data.files.concat(data.path)
+          });
         });
       }
     })
@@ -134,9 +139,14 @@ Page({
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
+        var formData = {
+          "dataType": "bus"
+        }
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
-          jcfiles: that.data.jcfiles.concat(res.tempFilePaths)
+        that.imgUpdate(res.tempFilePaths, formData, function (data) {
+          that.setData({
+            jcfiles: that.data.jcfiles.concat(data.path)
+          });
         });
       }
     })
@@ -148,14 +158,28 @@ Page({
     })
   },
   deleteImg: function (e){
-    console.log(e.currentTarget.dataset.index);
+    console.log(e.currentTarget.dataset);
     var index = e.currentTarget.dataset.index;
-    var files = this.data.files;
-    files.splice(index, 1);
-    this.setData({
-      files: files
-    });
+   
+    if (e.currentTarget.dataset.imgtype =="adImages"){
+      var files = this.data.files;
+      files.splice(index, 1);
+      this.setData({
+        files: files
+      });
+    
+    }
+    if (e.currentTarget.dataset.imgtype == "jcImages") {
+      var jcfiles = this.data.jcfiles;
+  
+      jcfiles.splice(index, 1);
+      this.setData({
+        jcfiles: jcfiles
+      });
+
+    }
   },
+  
   chooseLocation: function (e) {
     var that = this;
     wx.chooseLocation({
@@ -172,10 +196,9 @@ Page({
             longitude: res.longitude
           },
           success: function (res) {
-            console.log(res);
             that.setData({
               ad_info: res.result.ad_info,
-              address: res.result.address_component.street + res.result.address_component.street_number,
+              address: res.result.address_component.street + res.result.address_component.street_number.replace(res.result.address_component.street, ""),
               pcd: res.result.address_component.province + res.result.address_component.city + res.result.address_component.district,
               city: res.result.ad_info.province + "-" + res.result.ad_info.city + "-" + res.result.ad_info.city + "-" + res.result.ad_info.district,
               cityCode: res.result.ad_info.adcode,
@@ -192,16 +215,91 @@ Page({
     })
   },
   formSubmit(e) {
+    var that = this;
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
+
     var goods = e.detail.value;
-        // wx.request({
-        //   url: app.globalData.serverUrl + "/wx/user/"+app.globalData.appid +"/check-token",
-        //   data: {
-        //
-        //   },
-        //   success: function (res) {
-        //
-        //   }
-        // })
+    goods.adImages = JSON.stringify(that.data.files);
+    goods.jcImages = JSON.stringify(that.data.jcfiles);
+    var msg = ""
+    var reg = /^1[0-9]{10}$/;
+    if (!reg.test(goods.contactsTel)){
+      msg ="请输入正确的手机号"
+      return that.showModal(msg);
+    }
+    if (!goods.contacts) {
+      msg = "请输入联系人"
+      return that.showModal(msg);
+    }
+    if (!goods.name) {
+      msg = "请输入商户名称"
+      return that.showModal(msg);
+    }
+    if (!goods.city) {
+      msg = "请选择地址"
+      return that.showModal(msg);
+    }
+
+    if (!goods.facadeImage) {
+      msg = "请上传门面头照"
+      return that.showModal(msg);
+    }
+    if (!goods.licenseImage) {
+      msg = "请上传营业执照"
+      return that.showModal(msg);
+    }
+
+    if (!goods.address) {
+      msg = "请输入详细地址"
+      return that.showModal(msg);
+    }
+    if (that.data.files.length==0) {
+      msg = "请上传头部轮播图片"
+      return that.showModal(msg);
+    }
+    var token = wx.getStorageSync('token')
+    wx.request({
+      url: app.globalData.serverUrl + "/wx/business/" + app.globalData.appid + "/register  ",
+      method: 'POST',
+      header: {
+        'Authorization': token
+      },
+      data: goods,
+      success: (res) => {
+        console.log('*********************');
+        console.log(res.data);
+        console.log('*********************');
+      }
+    })
+   
+
   },
+  showModal:function(msg){
+    wx.showModal({
+      title: '提示',  
+      content: msg,
+      showCancel: false,
+      confirmText: '确定',
+    })
+  },
+  imgUpdate(tempFilePaths, formData,callBack){
+    var that = this;
+    wx.showLoading({
+      title: '上传中...',
+    })
+
+    // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+    wx.uploadFile({
+      url: app.globalData.serverUrl + "/wx/media/" + app.globalData.appid + "/uploadImg",
+      filePath: tempFilePaths[0],
+      name: 'img',
+      formData: formData,
+      success: function (res) {
+        var data = JSON.parse(res.data);
+        callBack(data);
+        wx.hideLoading();
+      }
+    })
+
+  }
 })
